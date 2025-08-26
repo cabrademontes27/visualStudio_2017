6a37f0c7116c681c11138a9b958d6d26502e953d
Luis Cabrera <cabrademontes@gmail.com>
2025-08-26 11:09:02 -0600

    class dog

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class ClasePerro
+    {
+        public String nombre { get; set; }
+        public int Edad { get; set; }
+
+        public ClasePerro(String _nombre, int _edad)
+        {
+            this.nombre = _nombre;
+            this.Edad = _edad;
+        }
+
+        public void ladrar()
+        {
+            Console.WriteLine("ladro");
+        }
+
+
+    }
+}
