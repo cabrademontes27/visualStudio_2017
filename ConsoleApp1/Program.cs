750bdfeaa687e7f541fa04d2f8ee21f61c545f0e
Luis Cabrera <cabrademontes@gmail.com>
2025-08-26 11:09:19 -0600

    change 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,11 +8,15 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
 
-            for (int i = 10; 1 > 11; i++)
+            Console.WriteLine("aqui si lo imprime");
+            for (int i = 0; i <= 10; i++)
             {
-                Console.WriteLine(i);
-                Console.WriteLine("new string");
+
+                Console.WriteLine("Hola dentro del for");
             }
+
+            ClasePerro clasePerro = new ClasePerro("Jose", 15);
+            clasePerro.ladrar();
         }
     }
 }
