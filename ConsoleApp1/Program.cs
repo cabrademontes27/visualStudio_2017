63551064de068a23715ca1ab9ed32f7d762ec45d
Luis Cabrera <LCabrera@ami.local>
2025-08-26 10:44:07 -0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int i = 10; 1 > 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
